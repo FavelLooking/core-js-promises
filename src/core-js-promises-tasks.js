874e7d7f65b3d2a32b8f0e1b60a79b15e7d4deb1
FavelLooking <intwestpol@gmail.com>
2024-04-14 19:52:16 +0400

    feat: solve few tasks

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -65,7 +65,18 @@ function getPromiseResult(source) {
  * [Promise.reject(1), Promise.reject(2), Promise.reject(3)]    => Promise rejected
  */
 function getFirstResolvedPromiseResult(promises) {
-  return Promise.race(promises).then((value) => value);
+  return new Promise((resolve, reject) => {
+    Promise.allSettled(promises).then((results) => {
+      const fulfilledPromises = results.filter(
+        (result) => result.status === 'fulfilled'
+      );
+      if (fulfilledPromises.length > 0) {
+        resolve(fulfilledPromises[0].value);
+      } else {
+        reject(new Error('All were rejected'));
+      }
+    });
+  });
 }
 
 /**
@@ -118,8 +129,12 @@ function getAllOrNothing(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+function getAllResult(promises) {
+  return Promise.allSettled(promises).then((results) =>
+    results.map((result) =>
+      result.status === 'fulfilled' ? result.value : null
+    )
+  );
 }
 
 /**
@@ -140,8 +155,25 @@ function getAllResult(/* promises */) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuPromises(promises) {
+  let result = '';
+  let promiseChain = Promise.resolve();
+
+  promises.forEach((promise) => {
+    promiseChain = promiseChain.then((value) => {
+      if (value !== undefined) {
+        result += value;
+      }
+      return promise;
+    });
+  });
+
+  return promiseChain.then((finalValue) => {
+    if (finalValue !== undefined) {
+      result += finalValue;
+    }
+    return result;
+  });
 }
 
 module.exports = {
